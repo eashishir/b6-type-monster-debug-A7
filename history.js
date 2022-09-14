3b3d28f3a866d5de68be615e1d68c87179adaa42
eashishir <eftekharahmedshishir@gmail.com>
2022-09-15 05:17:08 +0600

    almost done

--- history.js
+++ history.js
@@ -26,13 +26,17 @@ function displayHistory() {
   const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
 
   previousTests.forEach((test) => {
+
     const newRow = document.createElement("div");
     newRow.classList.add("card");
 
+
     newRow.innerHTML = `
+  
   <h3>${test.questionText}</h3>
   <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+  
   `;
 
     histories.appendChild(newRow);
